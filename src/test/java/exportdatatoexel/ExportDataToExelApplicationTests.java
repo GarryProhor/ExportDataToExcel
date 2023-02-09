44b410fb186eccdef3a9d34e76ef5e509805aef9
GarryProhor <igorprohorchenko@gmail.com>
2023-02-09 14:15:47 +0300

    Initial commit

---
+++ src/test/java/exportdatatoexel/ExportDataToExelApplicationTests.java
@@ -0,0 +1,13 @@
+package exportdatatoexel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportDataToExelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
